cb2e265014b41b43ef28b33d88a4941b33d59f54
naranja0047 <naranja@illunex.com>
2022-06-16 08:55:45 +0000

    modified readData, create TruncatedSVD

--- src/.ipynb_checkpoints/dataConnection-checkpoint.py
+++ src/.ipynb_checkpoints/dataConnection-checkpoint.py
@@ -1,41 +1,44 @@
-import pandas as pd
+import pandas as pd 
 
 class DataConnector:
     
-    def __init__(self, files: str):
-        """
-        detail description needed
-        """
-        print('Connecting')
+    def __init__(self, files):
         
+        """
+        Importing the dataset.
+        Define the data type for preprocessing easily later.
+        Put all the data into the dataframe.
+        """
+        
+        print('Connecting')        
         self.df = pd.DataFrame()
+        
         for file in files:
-            df_raw = pd.read_excel(file)
+            df_raw = pd.read_excel(file,dtype={'일자':str,'매출처':str,'코드':str,'수량': float,'합계': float})
             self.df=pd.concat([self.df, df_raw])
-        
-
-        
-    
-#     def connect():
-#         df_list = []
-#         for file in self:
-#             df_raw = pd.read_excel(file, 
-                    
-#                 )
-#             data=df_list.append(df_raw)
-#         return data 
-        
+            
     
     def df_preprocess(self):
+        
+        """
+        Preprocess the dataset to available for any algorythm.
+        """
+        
         df_raw = self.df
         df_raw.fillna("", inplace=True)
-        df_raw.rename(columns={"일자":"day","매출처":"customer","코드":"code","수량":"quantity", "단가":"price", "합계":"total", "금액":"sum", "세액":"tax", "비고":"etc"}, inplace=True)
-        df_raw = df_raw[df_raw["etc"].str.contains("취소")==False]
-        df_raw = df_raw[df_raw["etc"].str.contains("반품")==False]
+        df_raw = df_raw[df_raw["비고"].str.contains("취소")==False]
+        df_raw = df_raw[df_raw["비고"].str.contains("반품")==False]
+        df_raw.drop(columns=["No","품목명","규격","원산지","단위","단가","금액","세액","비고"], inplace=True)
+        df_raw.rename(columns={"일자":"day","매출처":"customer","코드":"product","수량":"quantity", "합계":"aggregate"}, inplace=True)
+
         df_raw = df_raw[df_raw["quantity"]!=0]
         df_raw = df_raw.drop(df_raw[df_raw["day"]=="소계"].index)
         df_raw = df_raw.drop(df_raw[df_raw["day"]=="합계"].index)
+        
+        df_raw["day"] = pd.to_datetime(df_raw["day"])
+        df_raw["ym"] = df_raw["day"].apply(lambda row : row.strftime("%Y%m"))
+        
         df = df_raw.copy()
-        df.drop(columns=["No","규격","단위","total","tax","etc"], inplace=True)
+        
         return df
     
