38c42c6e7b4ca2918f86d6aaf4f48ab5aae0352f
Joohyun Yoon <joohyunyoon@Joohyuns-MacBook-Pro.local>
2022-07-08 18:30:27 +0900

    modified code

--- singleton_test.py
+++ singleton_test.py
@@ -16,14 +16,13 @@ from utils import get_logger
 import warnings
 from tqdm import tqdm
 
-
 warnings.filterwarnings(action="ignore")
 logger = get_logger("learner_proxy")
 
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-def get_data(engine: sqlalchemy.engine.Engine) -> pd.DataFrame:
-    product_get_query = f'SELECT trans_date AS day, vendor_code AS customer, product_code AS product FROM transaction_list;'
+def get_vendor_data(engine: sqlalchemy.engine.Engine, vendor_id) -> pd.DataFrame:
+    product_get_query = f'SELECT trans_date, product_code AS product FROM transaction_list where vendor_code = {vendor_id};'
     result=pd.read_sql(product_get_query, engine)
     
     return result
@@ -41,165 +40,54 @@ def main(cfg: DictConfig) -> None:
         return -1
     print('connecting',{cfg.db})
 
-    customer_get_query=f'SELECT vendor_code AS customer FROM transaction_list;'
-    df_customer=pd.read_sql(customer_get_query, engine)
-    customer_list=set(df_customer['customer'].tolist())
-    del df_customer
+    vendor_get_query=f'SELECT DISTINCT vendor_code FROM transaction_list;'
+    vendor=pd.read_sql(vendor_get_query, engine)
+    del vendor_get_query
     
-    for customer_id in tqdm(customer_list):
-        unique_customer=customer_id  
-        print(unique_customer)
-        print(len(unique_customer))
-    # df_customer = get_data(engine)
-    # df_customer["ym"] = df_customer["day"].apply(lambda row : row.strftime("%Y%m"))
-    # df_dataset=df_customer[df_customer['customer']==unique_customer]
-    # print(df_dataset)   
+    for index, row in tqdm(vendor.iterrows()):
+        # print(row['vendor_code'])
+        vendor_code=row['vendor_code']
+        vendor_data=get_vendor_data(engine, vendor_code)
+        print(vendor_data)
+        preprocess(vendor_data, vendor_code, 3, cfg.output_dir)
+
+def preprocess(vendor_data, vendor_code, input_months:int, output_dir: str)->None:
+
+    vendor_data["ym"] = vendor_data["trans_date"].apply(lambda row : row.strftime("%Y%m"))
+    # print(vendor_data)   
     
-    # if input_months == 3:
-    #         input_3months = str(int(max(result["ym"]))-int(2))
-    #     splited_months=store[store["ym"] >= input_3months]
+    if input_months == 3:
+            input_3months = str(int(max(vendor_data["ym"]))-int(2))
+            splited_months=vendor_data[vendor_data["ym"] >= input_3months]
         
-    # elif input_months == 6:
-    #     input_6months = str(int(max(result["ym"]))-int(5))
-    #     splited_months=store[store["ym"] >= input_6months]
+    elif input_months == 6:
+        input_6months = str(int(max(vendor_data["ym"]))-int(5))
+        splited_months=vendor_data[vendor_data["ym"] >= input_6months]
         
-    # else:
-    #     print('input only 3 or 6')
+    else:
+        print('input only 3 or 6')
             
-
-    # quantile_75=(splited_months['product'].value_counts()).quantile(q=0.75)
-    # quantile_proprecessed=splited_months[splited_months['product'].map(splited_months['product'].value_counts())>quantile_75]
-    # product_preprecessed=quantile_proprecessed.groupby('ym').agg(list)
+    quantile_75=(splited_months['product'].value_counts()).quantile(q=0.75)
+    quantile_proprecessed=splited_months[splited_months['product'].map(splited_months['product'].value_counts())>quantile_75]
+    product_preprecessed=quantile_proprecessed.groupby('ym').agg(list)
     
-    # dataset=[]
-    # for c,p in product_preprecessed.iterrows():
-    #     dataset.append(p['product'])
+    dataset=[]
+    for c,p in product_preprecessed.iterrows():
+        dataset.append(p['product'])
+    te = TransactionEncoder()
+    te_ary = te.fit(dataset).transform(dataset)
+    single_dataset = pd.DataFrame(te_ary, columns=te.columns_)    
+    
+    frequent_itemsets = apriori(single_dataset, min_support = 0.1, max_len = 2, use_colnames=True)
         
-    # te = TransactionEncoder()
-    # te_ary = te.fit(dataset).transform(dataset)
-    # single_dataset = pd.DataFrame(te_ary, columns=te.columns_)    
+    trained_model = association_rules(frequent_itemsets).sort_values(by=['lift'],ascending=False)
+
+    print(f"trained_model : {trained_model}")
     
-    # frequent_itemsets = apriori(single_dataset, min_support = 0.1, max_len = 2, use_colnames=True)
-        
-    # trained_model = association_rules(frequent_itemsets).sort_values(by=['lift'],ascending=False)
+    train_file_path = f'{output_dir}/{vendor_code}_model.bin'
 
-    # print(f"trained_model : {trained_model}")
-    
-    # train_file_path = f'{output_dir}/{store_id}_model.bin'
-
-    # with open(train_file_path,'wb') as file:
-    #     pkl.dump(trained_model, file)        
-    
-    
-#         print(df_customer)
-        
-        # trainer(3,df_customer,cfg.output_dir)
-        
-        
-# def trainer(
-#     input_months:int,
-#     store_id: int,
-#     output_dir: str) -> None:
-    
-    
-#     result["ym"] = result["day"].apply(lambda row : row.strftime("%Y%m"))
-#     store=result[result['customer']==store_id]
-#     store=store[['customer','product','ym']]
-    
-
-#     if input_months == 3:
-#         input_3months = str(int(max(result["ym"]))-int(2))
-#         splited_months=store[store["ym"] >= input_3months]
-        
-#     elif input_months == 6:
-#         input_6months = str(int(max(result["ym"]))-int(5))
-#         splited_months=store[store["ym"] >= input_6months]
-        
-#     else:
-#         print('input only 3 or 6')
-            
-
-#     quantile_75=(splited_months['product'].value_counts()).quantile(q=0.75)
-#     quantile_proprecessed=splited_months[splited_months['product'].map(splited_months['product'].value_counts())>quantile_75]
-#     product_preprecessed=quantile_proprecessed.groupby('ym').agg(list)
-    
-#     dataset=[]
-#     for c,p in product_preprecessed.iterrows():
-#         dataset.append(p['product'])
-        
-#     te = TransactionEncoder()
-#     te_ary = te.fit(dataset).transform(dataset)
-#     single_dataset = pd.DataFrame(te_ary, columns=te.columns_)    
-    
-#     frequent_itemsets = apriori(single_dataset, min_support = 0.1, max_len = 2, use_colnames=True)
-        
-#     trained_model = association_rules(frequent_itemsets).sort_values(by=['lift'],ascending=False)
-
-#     print(f"trained_model : {trained_model}")
-    
-#     train_file_path = f'{output_dir}/{store_id}_model.bin'
-
-#     with open(train_file_path,'wb') as file:
-#         pkl.dump(trained_model, file)        
-        
-
-
-# class Singleton(Training):
-#     has_inst = False
-#     def __new__(cls, *args, **kwargs):
-#         if cls.has_inst:
-#             raise ValueError('cannot create more objects')
-#         cls.has_inst = True
-#         print(f"super()2 : {super()}")
-#         return super().__new__(cls)
-    
-# p1=Singleton()
-# print(p1)
-
-# class test1():
-#     def __init__(self):
-#         print("test1 init")
-
-# class test2(test1):
-#     def __init__(self):
-#         print("test2 init")
-#         print(f"super : {super()}")
-#         super().__init__()
-
-# p=test2()
-# print(p)
-
-# p2=Singleton()
-# print(p2)
-
-# class Person:
-#     def __new__(cls, firstname, lastname) :
-#         obj= super().__new__(cls)
-#         obj.firstname=firstname
-#         obj.lastname=lastname
-        
-#         obj.fullname=f'{firstname}{lastname}'
-#         return obj
-    
-# p=Person('joohyun','yoon')
-# print(p.fullname)
-# print(p.__dict__)
-
-# _singleton = None
-
-# class Example:
-#     def __new__(cls):
-#         global _singleton
-
-#         if _singleton is None:
-#             _singleton = super(Example, cls).__new__(cls)
-
-#         return _singleton
-
-# a = Example()
-# b = Example()
-# print(type(a))
-# print(type(Example))
+    with open(train_file_path,'wb') as file:
+        pkl.dump(trained_model, file)        
 
 if __name__ == "__main__":
     main()
